1287a57c1066bc4d2e0bd6442a2f8f402fa14da8
MoisesHC <genesis2204gamerhc@gmail.com>
2024-10-22 19:46:34 -0600

    RO-Sprint 1_Moises:Integración de la estructura del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, RouterLink } from '@angular/router';
+import { faHouse, faBagShopping, faShirt, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterModule,
+    FontAwesomeModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'RentOutfit';
+
+  faHouse = faHouse;
+  faBag = faBagShopping;
+  faShirt = faShirt;
+  faSearch = faMagnifyingGlass;  
 }
